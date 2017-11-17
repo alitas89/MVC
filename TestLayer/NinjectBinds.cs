762934e167a17e86cb36a17ac98dc35fb0aa1cbb
Eren Güvercin <egvrcn@gmail.com>
2017-11-17 09:07:14 +0300

    'v1.06'

---
+++ TestLayer/NinjectBinds.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Dapper;
+using Ninject.Modules;
+using WebApi.Controllers;
+
+namespace TestLayer
+{
+    public class NinjectBinds: NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ITestService>().To<TestManager>();
+            Bind<ITestDal>().To<DpTestDal>();
+        }
+    }
+}
---
+++ TestLayer/Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using Moq;
+using NUnit.Framework;
+using WebApi.App_Start;
+using WebApi.Controllers;
+using Ninject;
+
+namespace TestLayer
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void IpFourDotItem()
+        {
+            //Arrange
+
+            var kernel = new StandardKernel(new NinjectBinds());
+            var controllerTest = kernel.Get<TestController>();
+
+            controllerTest.Request = new HttpRequestMessage();
+            controllerTest.Configuration = new System.Web.Http.HttpConfiguration();
+
+            //Act
+            var response = controllerTest.Get(1);
+
+            //Assert
+            var fourDotItemForIp = response.Split('.');
+            
+            Assert.AreEqual(4, fourDotItemForIp.Length);
+        }
+    }
+}
