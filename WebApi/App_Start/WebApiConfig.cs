280c09a6b254d43868dfdc889f55d24d7a2f5013
Ferhat Ateş <ferhatateses@gmail.com>
2017-11-22 22:14:31 +0300

    v1.2

--- WebApi/App_Start/WebApiConfig.cs
+++ WebApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using UtilityLayer.Filters;
 
 namespace WebApi
 {
@@ -12,6 +13,8 @@ namespace WebApi
             // Web API configuration and services
 
             // Web API routes
+            config.MessageHandlers.Add(new ApiResponseHandler());
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
