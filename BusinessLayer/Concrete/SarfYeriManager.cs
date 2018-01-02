a984b2f73f89590e5ad7fcf85dd72b101d636920
egvrcn <egvrcn@gmail.com>
2018-01-02 16:04:45 +0300

    'v1.45'

--- BusinessLayer/Concrete/SarfYeriManager.cs
+++ BusinessLayer/Concrete/SarfYeriManager.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BusinessLayer.Abstract;
+using BusinessLayer.ValidationRules.FluentValidation;
+using Core.Aspects.Postsharp.AuthorizationAspects;
+using Core.Aspects.Postsharp.CacheAspects;
+using Core.Aspects.Postsharp.ValidationAspects;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
 using DataAccessLayer.Abstract.DataAccessLayer.Abstract;
 using EntityLayer.Concrete;
 
@@ -18,26 +23,44 @@ namespace BusinessLayer.Concrete
             _sarfyeriDal = sarfyeriDal;
         }
 
+        [CacheAspect(typeof(MemoryCacheManager))]
+        [SecuredOperation(Roles = "Admin,Editor")]
         public List<SarfYeri> GetList()
         {
             return _sarfyeriDal.GetList();
         }
+
+        [SecuredOperation(Roles = "Admin,Editor")]
         public SarfYeri GetById(int Id)
         {
             return _sarfyeriDal.Get(Id);
         }
+
+        [CacheRemoveAspect(typeof(MemoryCacheManager))]
+        [FluentValidationAspect(typeof(SarfYeriValidator), AspectPriority = 1)]
+        [SecuredOperation(Roles = "Admin,Editor")]
         public int Add(SarfYeri sarfyeri)
         {
             return _sarfyeriDal.Add(sarfyeri);
         }
+
+        [CacheRemoveAspect(typeof(MemoryCacheManager))]
+        [FluentValidationAspect(typeof(SarfYeriValidator), AspectPriority = 1)]
+        [SecuredOperation(Roles = "Admin,Editor")]
         public int Update(SarfYeri sarfyeri)
         {
             return _sarfyeriDal.Update(sarfyeri);
         }
+
+        [CacheRemoveAspect(typeof(MemoryCacheManager))]
+        [SecuredOperation(Roles = "Admin,Editor")]
         public int Delete(int Id)
         {
             return _sarfyeriDal.Delete(Id);
         }
+
+        [CacheRemoveAspect(typeof(MemoryCacheManager))]
+        [SecuredOperation(Roles = "Admin,Editor")]
         public int DeleteSoft(int Id)
         {
             return _sarfyeriDal.DeleteSoft(Id);
---
+++ BusinessLayer/ValidationRules/FluentValidation/SarfYeriValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules.FluentValidation
+{
+    class SarfYeriValidator : AbstractValidator<SarfYeri>
+    {
+        public SarfYeriValidator()
+        {
+            RuleFor(p => p.Kod).NotEmpty().WithMessage("SarfYeri kodu boş bırakılamaz!");
+            RuleFor(p => p.Kod).MaximumLength(50).WithMessage("SarfYeri Kodu 50 karakterden fazla olamaz!");
+        }
+    }
+}
