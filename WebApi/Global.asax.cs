3c018336bd3fc72761d0e38923f0bbb44a78f17c
egvrcn <egvrcn@gmail.com>
2018-01-14 12:45:34 +0300

    'v1.53'

--- WebApi/Global.asax.cs
+++ WebApi/Global.asax.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using WebApi.MediaTypes;
 
 namespace WebApi
 {
@@ -12,6 +14,9 @@ namespace WebApi
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            //Api url bilgisine ek olarak format belirtlmesiyle ilgili yapı devreye girer ve ona uygun olarak bir çıktı üretir. url?format=csv
+            GlobalConfiguration.Configuration.Formatters.Add(new CsvFormatter(new QueryStringMapping("format", "csv", "text/csv")));
         }
     }
 }
