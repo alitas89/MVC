8d6a4dc18924a8b276e5887c0b5a0cb9f6bf5279
egvrcn <egvrcn@gmail.com>
2018-03-31 15:23:05 +0300

    'v2.21'

---
+++ WebApi/Controllers/InfoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace WebApi.Controllers
+{
+    public class InfoController : ApiController
+    {
+        [Route("api/info/kullaniciadi")]
+        [HttpGet]
+        public string KullaniciAdi()
+        {
+            return ClaimsPrincipal.Current.Identity.Name;
+        }
+
+        [Route("api/info/kullaniciadsoyad")]
+        [HttpGet]
+        public string KullaniciAdSoyad()
+        {
+            var actor = ((ClaimsIdentity)User.Identity).Claims.FirstOrDefault(x =>
+                x.Type.Substring(x.Type.LastIndexOf('/'), x.Type.Length - x.Type.LastIndexOf('/')) == "/actor")?.Value;
+            return actor;
+        }
+
+        [Route("api/info/roles")]
+        [HttpGet]
+        public string Roles()
+        {
+            //ClaimsPrincipal principal = Request.GetRequestContext().Principal as ClaimsPrincipal;
+            List<string> listRole = new List<string>();
+            var identity = (ClaimsIdentity)User.Identity;
+            IEnumerable<Claim> claims = identity.Claims;
+            foreach (var claim in claims)
+            {
+                string name = claim.Type.Substring(claim.Type.LastIndexOf('/'),
+                    claim.Type.Length - claim.Type.LastIndexOf('/'));
+                if (name=="/role")
+                {
+                    listRole.Add(claim.Value);
+                }
+            }
+            return JsonConvert.SerializeObject(listRole);
+        }
+    }
+}
