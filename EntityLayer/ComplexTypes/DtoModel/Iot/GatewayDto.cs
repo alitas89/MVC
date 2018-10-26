f0fc5916be81602247e4941c573c190a2adbfa6d
cagrierimez <cagrierimez@gmail.com>
2018-10-26 17:12:08 +0300

    v3.09

--- EntityLayer/ComplexTypes/DtoModel/Iot/GatewayDto.cs
+++ EntityLayer/ComplexTypes/DtoModel/Iot/GatewayDto.cs
@@ -9,8 +9,8 @@ namespace EntityLayer.ComplexTypes.DtoModel.Iot
         public string EnerjiDurumu { get; set; }
         public string Sicaklik { get; set; }
         public int OkunanSayac { get; set; }
-        public string Latitude { get; set; }
-        public string Longitude { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
         public int VarlikID { get; set; }
     }
 }
